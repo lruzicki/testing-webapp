7346c3a90593243bf3cff430b5064ed2c1ebd171
Damian Borowiecki <dborowiecki@soldevelo.com>
2023-01-25 11:31:45 +0100

    TECH-214: Aligned with airbnb code style

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,12 +1,12 @@
-const { MongoClient } = require('mongodb')
+const { MongoClient } = require('mongodb');
 
-require('dotenv').config()
+require('dotenv').config();
 
 class ClientBuilder {
   constructor() {
-    this.conn = this.getConnection()
-    this.connectionURI = this.buildUri()
-    this.databaseName = this.conn.databaseName
+    this.conn = this.getConnection();
+    this.connectionURI = this.buildUri();
+    this.databaseName = this.conn.databaseName;
   }
 
     getConnection = () => {
@@ -16,43 +16,42 @@ class ClientBuilder {
         host: process.env.MONGO_HOST,
         port: process.env.MONGO_PORT,
         databaseName: process.env.MONGO_DATABASE,
-      }
+      };
 
-      const undefinedFields = Object.keys(conn).filter((item) => !conn[item])
+      const undefinedFields = Object.keys(conn).filter((item) => !conn[item]);
       if (undefinedFields.length > 0) {
-        throw new Error(`Some of mandatory environmental variables are missing [${undefinedFields}]`)
+        throw new Error(`Some of mandatory environmental variables are missing [${undefinedFields}]`);
       }
 
-      return conn
+      return conn;
     }
 
     buildUri = () => {
-      const uri =
-          'mongodb://'+
-          `${this.conn.username}:${encodeURIComponent(this.conn.passwd)}@`+
-          `${this.conn.host}:${this.conn.port}`+
-          '/?maxPoolSize=20&w=majority'
+      const uri = 'mongodb://'
+          + `${this.conn.username}:${encodeURIComponent(this.conn.passwd)}@`
+          + `${this.conn.host}:${this.conn.port}`
+          + '/?maxPoolSize=20&w=majority';
 
-      return uri
+      return uri;
     }
 }
 
-let db = null
+let db = null;
 async function getDatabase() {
   if (!db) {
-    const connectionDetails = new ClientBuilder()
+    const connectionDetails = new ClientBuilder();
     const client = new MongoClient(connectionDetails.connectionURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 2000
-    })
-    await client.connect()
-    db = await client.db(connectionDetails.databaseName)
+      serverSelectionTimeoutMS: 2000,
+    });
+    await client.connect();
+    db = await client.db(connectionDetails.databaseName);
   }
 
-  return db
+  return db;
 }
 
-module.exports =  {
-  db: getDatabase()
-}
+module.exports = {
+  db: getDatabase(),
+};
--- server/server.js
+++ server/server.js
@@ -1,30 +1,28 @@
-const express = require('express')
-const cors = require('cors')
-const { apiKeyAuth } = require('@vpriem/express-api-key-auth')
-const { db } = require('./db/conn')
+const express = require('express');
+const cors = require('cors');
+const { apiKeyAuth } = require('@vpriem/express-api-key-auth');
+const { db } = require('./db/conn');
 
-const port = process.env.PORT || 5000
-const app = express()
+const port = process.env.PORT || 5000;
+const app = express();
 
-app.use(cors())
-app.use('/report/upload', apiKeyAuth(/^API_KEY_/)) // Matching all process.env.API_KEY_*
+app.use(cors());
+app.use('/report/upload', apiKeyAuth(/^API_KEY_/)); // Matching all process.env.API_KEY_*
 
-app.use(express.json())
-//app.use(require('./routes/record'))
+app.use(express.json());
+// app.use(require('./routes/record'))
 
-db.then(db =>{
+db.then((dataBase) => {
   try {
-    const reportCollection = db.collection('testexecutions')
-    app.locals.reportCollection = reportCollection
-    console.log('Database connected', db.databaseName)
+    const reportCollection = dataBase.collection('testexecutions');
+    app.locals.reportCollection = reportCollection;
+    console.log('Database connected, %s', dataBase.databaseName);
+  } catch (error) {
+    console.err(error);
+    throw Error(error);
   }
-  catch (error) {
-    console.err(error)
-    throw Error(error)
-  }
-
-}).catch(err => console.error('Database connection failed, reason:\n', err))
+}).catch((err) => console.error('Database connection failed, reason:\n', err));
 
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`)
-})
+  console.log(`Server is running on port: ${port}`);
+});
