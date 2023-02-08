deddc45add8ee80cb63f75da9be222af1a0d05f1
Karolina Kopacz <karolina.kopacz94@gmail.com>
2023-02-08 16:59:40 +0100

    TECH-273: Fix less config with next.js, moved variables and extends styles to separate files

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPlugins = require('next-compose-plugins');
+const withLess = require('next-with-less');
+
+const plugins = [
+  [
+    withLess,
+    {
+      lessLoaderOptions: {},
+    },
+  ],
+];
+
+module.exports = withPlugins(plugins, {
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+});
