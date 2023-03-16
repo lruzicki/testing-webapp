1f5424f89d0007057ae303cab7fc3d4ba9b0d66a
Paweł Gesek <pgesek@soldevelo.com>
2023-03-16 15:26:03 +0100

    TECH-434: Cd to script dir

--- deployment/run-prod.sh
+++ deployment/run-prod.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
 # Do not use the override compose file, which is intended for development
 # We run in prod mode, but we still build from this guild repository
 docker-compose -f ../docker-compose.yml up --build -d --force-recreate 
