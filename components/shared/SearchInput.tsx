1697ead5b4c28cb6bf4b126b72a85bf391b154a7
Karolina Kopacz <karolina.kopacz94@gmail.com>
2023-04-11 17:01:44 +0200

    TECH-514: Added 'search; to translations, moved css style to the classNames inside async select

--- components/shared/SearchInput.tsx
+++ components/shared/SearchInput.tsx
@@ -1,86 +1,49 @@
 import { RiSearchLine } from 'react-icons/ri';
 import dynamic from 'next/dynamic';
-import {  GroupBase, StylesConfig } from 'react-select';
+import { useIntl } from 'react-intl';
+import { useCallback } from 'react';
 
 const AsyncReactSelect = dynamic(() => import('react-select/async'), {
   ssr: false,
 });
 
-const defaultStyles: StylesConfig<unknown, boolean, GroupBase<unknown>> | undefined = {
-  dropdownIndicator: () => ({
-    display: 'none',
-  }),
-  indicatorSeparator: () => ({
-    display: 'none',
-  }),
-  placeholder: (provided) => ({
-    ...provided,
-    color: 'rgba(193, 193, 193, 1)',
-  }),
-  valueContainer: (provided) => ({
-    ...provided,
-    padding: '8px 22px 8px 45px',
-    maxWidth: '100%',
-    overflow: 'hidden',
-  }),
-  container: (provided) => ({
-    ...provided,
-    boxShadow: 'none',
-    outline: 'none',
-    borderRadius: '4px',
-    border: '1px solid rgba(223, 223, 234, 1)',
-    width: '300px',
-    height: '42px',
-  }),
-  control: (provided) => ({
-    ...provided,
-    boxShadow: 'none',
-    border: 0,
-    fontSize: '14px',
-    color: 'rgba(193, 193, 193, 1)',
-    overflow: 'hidden',
-  }),
-  option: (provided, isFocused) => ({
-    ...provided,
-    color: '#282733',
-    fontSize: '14px',
-    backgroundColor: isFocused ? 'transparent' : 'transparent',
-    height: '42px',
-    paddingLeft: '16px',
-  }),
-  input: (provided) => ({
-    ...provided,
-    overflow: 'hidden',
-  }),
-};
-
 type Props = {
   value?: string;
-  placeholder?: string;
   name: string;
 };
 
-const SearchInput = ({
-  value,
-  placeholder = 'Search',
-  name,
-  ...otherProps
-}: Props) => (
-  <div className='search-input-container'>
-    <AsyncReactSelect
-      {...otherProps}
-      cacheOptions
-      inputId={`async-select-id-${name}`}
-      value={value}
-      placeholder={placeholder}
-      classNamePrefix='react-select'
-      className=''
-      styles={defaultStyles}
-    />
-    <div className='search-input-loupe'>
-      <RiSearchLine />
+const SearchInput = ({ value, name,...otherProps }: Props) => {
+  const { formatMessage } = useIntl();
+  const format = useCallback(
+    (id: string) => formatMessage({ id }),
+    [formatMessage]
+  );
+
+  return (
+    <div className='search-input-container'>
+      <AsyncReactSelect
+        {...otherProps}
+        cacheOptions
+        inputId={`async-select-id-${name}`}
+        value={value}
+        placeholder={format('app.search')}
+        unstyled
+        classNames={{
+          dropdownIndicator: () => 'search-dropdown-indicator',
+          valueContainer: () => 'search-value-container',
+          placeholder: () => 'search-placeholder',
+          container: () => 'search-container',
+          menuList: () => 'search-menu-list',
+          option: () => 'search-option',
+          input: () => 'search-input',
+          menu: () => 'search-menu',
+        }}
+      />
+      <div className='search-input-loupe'>
+        <RiSearchLine />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default SearchInput;
