99e2e2c22803f83f6a682d89fbaa85255302f931
Damian Borowiecki <dborowiecki@soldevelo.com>
2023-08-24 12:25:58 +0200

    Frontend docker use build instead of dev

--- next.config.js
+++ next.config.js
@@ -23,6 +23,22 @@ const nextConfig = {
   eslint: {
     dirs: ['pages', 'utils', 'app', 'components', 'src', '__tests__'],
   },
+  env: {
+    API_URL: process.env.API_URL,
+  },
+
+  webpack(config, { dev, isServer }) {
+    // If not in development mode, avoid minification
+    // Currently minification breaks production build for the table
+    // Error: Application error: a client-side exception has occurred (see the browser console for more information)
+    if (!dev) {
+      config.optimization.minimize = false;
+      // Disable minifying JavaScript
+      config.optimization.minimizer = [];
+    }
+
+    return config;
+  },
 };
 
 module.exports = withPlugins(plugins, nextConfig);
