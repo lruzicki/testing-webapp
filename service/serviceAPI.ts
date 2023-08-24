ea731960366f70df988fee6b1eb3a2968ad7ee60
Damian Borowiecki <dborowiecki@soldevelo.com>
2023-08-24 13:43:04 +0200

    TECH-813: Display only results from main branch

--- service/serviceAPI.ts
+++ service/serviceAPI.ts
@@ -34,12 +34,14 @@ const baseFetch = async (url: string): Promise<ProductsListType> => {
 
 export const getSoftwaresData = async (
   offset: number,
-  sortBy: SoftwaresTableSortByType
+  sortBy: SoftwaresTableSortByType,
+  branch: string
 ): Promise<Success<ProductsListType> | Failure> => {
   try {
     const url = new URL(`${baseUrl}/report/`);
     url.searchParams.set('limit', '20');
     url.searchParams.set('offset', offset.toString());
+    url.searchParams.set('branch', branch || 'main');
 
     const sortedParameters = handleFieldsToSort(sortBy);
     for (const param of sortedParameters.split('&')) {
@@ -58,32 +60,37 @@ export const getSoftwaresData = async (
   }
 };
 
-export const getSoftwareListCount = async () => {
-  return await fetch(`${baseUrl}/report/count`, {
-    method: 'get',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then(async (response) => {
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
+export const getSoftwareListCount = async (branch: string): Promise<Success<number> | Failure> => {
+  try {
+    const url = new URL(`${baseUrl}/report/count`);
 
-      return response.json();
-    })
-    .then<Success<number>>((actualCount) => {
-      return {
-        status: true,
-        data: actualCount.count,
-      };
-    })
-    .catch<Failure>((error) => {
-      return {
-        status: false,
-        error,
-      };
+    // Assuming branch is a query parameter. If not, this can be removed.
+    const params = new URLSearchParams({ branch: branch || 'main' });
+    url.search = params.toString();
+
+    const response = await fetch(url.toString(), {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    const result = await response.json();
+
+    return {
+      status: true,
+      data: result.count,
+    };
+  } catch (error) {
+    return {
+      status: false,
+      error: error as Error,
+    };
+  }
 };
 
 export const getBuildingBlockTestResults = async (
